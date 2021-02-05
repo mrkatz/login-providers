183c1c326328e4a638067d17b0ef1a8f3b009393
mrkatz <adamkaczocha@gmail.com>
2021-02-05 18:35:37 +1100

    Add Test Framework
    Framework with Initial Test

--- tests/Feature/InitialTest.php
+++ tests/Feature/InitialTest.php
@@ -2,10 +2,21 @@
 
 namespace Mrkatz\LoginProviders\Tests;
 
+use Mrkatz\LoginProviders\LoginProvidersServiceProvider;
 use Orchestra\Testbench\TestCase;
 
 class InitialTest extends TestCase
 {
+    /**
+     * Load package service provider.
+     * @param \Illuminate\Foundation\Application $app
+     * @return LoginProvidersServiceProvider
+     */
+    protected function getPackageProviders($app)
+    {
+        return [LoginProvidersServiceProvider::class];
+    }
+
     /** @test */
     public function testBasicTest()
     {
